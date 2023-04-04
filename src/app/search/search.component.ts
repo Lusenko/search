6d14b4966dab467fbf54799c695b286b282d2573
Lusenko <lusenko.sasha.2015@gmail.com>
2023-04-04 15:39:27 +0300

    feat: add some logic for search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -12,8 +12,9 @@ import {Info} from "../../interface/info";
 })
 export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('dropdown') dropdown: ElementRef | undefined;
+  @ViewChild('input') input: ElementRef | undefined;
 
-  search = new FormControl('');
+  search = new FormControl('', {nonNullable: true});
 
   selectTitleIndex = 0;
   selectValue = '';
@@ -49,45 +50,67 @@ export class SearchComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     fromEvent<KeyboardEvent>(window, 'keyup').pipe(
-      tap((event: KeyboardEvent) => {
+      filter((event: KeyboardEvent) => event.key === 'ArrowDown'),
+      tap(() => {
         const selectTitle = document.querySelector('.focus') as HTMLDivElement;
         const lastTitleIndex = this.titleList.length - 1;
 
-        if(event.key === 'ArrowDown') {
-          this.selectTitleIndex++;
+        this.selectTitleIndex++;
 
-          if(this.dropdown) {
-            if(selectTitle) {
-              this.dropdown.nativeElement.scrollTop = selectTitle.offsetTop - 250;
-            }
-
-            if(this.selectTitleIndex > lastTitleIndex) {
-              this.selectTitleIndex = 0;
-              this.dropdown.nativeElement.scrollTop = 0;
-            }
+        if(this.dropdown) {
+          if(selectTitle) {
+            this.dropdown.nativeElement.scrollTop = selectTitle.offsetTop - 250;
           }
-        }
 
-        if(event.key === 'ArrowUp') {
-          this.selectTitleIndex--;
-
-          if(this.dropdown) {
-            if(selectTitle) {
-              this.dropdown.nativeElement.scrollTop = selectTitle.offsetTop - 250;
-            }
-
-            if(this.selectTitleIndex === -1) {
-              this.selectTitleIndex = lastTitleIndex;
-              this.dropdown.nativeElement.scrollTop = this.dropdown.nativeElement.scrollHeight;
-            }
+          if(this.selectTitleIndex > lastTitleIndex) {
+            this.selectTitleIndex = 0;
+            this.dropdown.nativeElement.scrollTop = 0;
           }
-        }
-
-        if(event.key === 'Enter') {
-          this.selectValue = this.titleList[this.selectTitleIndex].title;
-          this.isShowDropdown = false;
         }
       }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'ArrowUp'),
+      tap(() => {
+        const selectTitle = document.querySelector('.focus') as HTMLDivElement;
+        const lastTitleIndex = this.titleList.length - 1;
+
+        this.selectTitleIndex--;
+
+        if (this.dropdown) {
+          if (selectTitle) {
+            this.dropdown.nativeElement.scrollTop = selectTitle.offsetTop - 250;
+          }
+
+          if (this.selectTitleIndex === -1) {
+            this.selectTitleIndex = lastTitleIndex;
+            this.dropdown.nativeElement.scrollTop = this.dropdown.nativeElement.scrollHeight;
+          }
+        }
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'Enter'),
+      tap(() => {
+        this.selectValue = this.titleList[this.selectTitleIndex].title;
+        this.isShowDropdown = false;
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent(window, 'click').pipe(
+      filter(e => e.target !== this.dropdown?.nativeElement && e.target !== this.input?.nativeElement),
+      tap(() => this.isShowDropdown = false),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent(window, 'click').pipe(
+      filter(e => e.target === this.input?.nativeElement && this.search.value !== ''),
+      tap(() => this.isShowDropdown = true),
       takeUntil(this.unsubscribe$),
     ).subscribe()
   }
