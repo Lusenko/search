da94234f96518239b6b64da6cd3f20753727294e
Lusenko <lusenko.sasha.2015@gmail.com>
2023-04-11 16:23:57 +0300

    feat(color-picker): add event on keyboard

--- src/app/main/color-picker/drop-down-list/drop-down-list.component.ts
+++ src/app/main/color-picker/drop-down-list/drop-down-list.component.ts
@@ -1,16 +1,19 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
 import {Color} from "../../../../interface/color";
+import {filter, fromEvent, Subject, takeUntil, tap} from "rxjs";
 
 @Component({
   selector: 'app-drop-down-list',
   templateUrl: './drop-down-list.component.html',
   styleUrls: ['./drop-down-list.component.scss']
 })
-export class DropDownListComponent implements OnInit, AfterViewInit {
+export class DropDownListComponent implements  AfterViewInit, OnDestroy {
   @Input() colorList: Color[] = [];
   @Output() selectedColor = new EventEmitter<string>();
 
   colorIndex = -1;
+
+  private unsubscribe$ = new Subject<void>();
 
   constructor() { }
 
@@ -19,10 +22,51 @@ export class DropDownListComponent implements OnInit, AfterViewInit {
     this.selectedColor.emit(this.colorList[index].color);
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'ArrowDown'),
+      tap(() => {
+        this.colorIndex > this.colorList.length - 5 ? this.colorIndex -= this.colorList.length - 5 : this.colorIndex += 5;
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'ArrowUp'),
+      tap(() => {
+        this.colorIndex < 5 ? this.colorIndex += this.colorList.length - 5 : this.colorIndex -= 5;
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'ArrowRight'),
+      tap(() => {
+        this.colorIndex++;
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'ArrowLeft'),
+      tap(() => {
+        this.colorIndex--;
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
+
+    fromEvent<KeyboardEvent>(window, 'keyup').pipe(
+      filter((event: KeyboardEvent) => event.key === 'Enter'),
+      tap(() => {
+        this.selectedColor.emit(this.colorList[this.colorIndex].color);
+      }),
+      takeUntil(this.unsubscribe$),
+    ).subscribe()
   }
 
-  ngAfterViewInit(): void {
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
