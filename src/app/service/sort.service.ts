ecb36bcebe6b68a4ec9a03fea178ccbdf1c1b775
Lusenko <lusenko.sasha.2015@gmail.com>
2023-05-11 17:25:17 +0300

    fix: change default sorting

--- src/app/service/sort.service.ts
+++ src/app/service/sort.service.ts
@@ -11,11 +11,7 @@ export class SortService {
   sortTable(header: TableHeader<Post>, list: Post[]): Post[] {
     switch (header.sortedState) {
       case SortState.default:
-        return list.sort((a,b) => {
-          const key = header.head as keyof Post;
-
-          return a[key] > b[key] ? 1 : -1;
-        });
+        return list.sort((a,b) => a.id > b.id ? 1 : -1);
 
       case SortState.up:
         return list.sort((a,b) => {
