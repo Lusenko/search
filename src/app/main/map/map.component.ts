c805b01d0ce924251056789401b5f5624fb263ef
Lusenko <lusenko.sasha.2015@gmail.com>
2023-05-23 18:38:02 +0300

    feat: add displayed map

--- src/app/main/map/map.component.ts
+++ src/app/main/map/map.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+import {FormBuilder} from "@angular/forms";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-map',
@@ -6,11 +9,40 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  coordinateForm = this.formBuilder.group({
+    latitude: '',
+    longitude: ''
+  })
+
+  private map: L.Map;
+  private unsubscribe$ = new Subject<void>();
+  constructor(private readonly formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createMap();
+  }
+
+  createMap(): void {
+    const coordinates = {
+      lat: 48.539927079257815,
+      lng: 32.53492066116194,
+    };
+
+    const zoomLevel = 12;
+
+    this.map = L.map('map').setView([coordinates.lat, coordinates.lng], zoomLevel);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 12,
+      maxZoom: 17,
+    }).addTo(this.map);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
--- src/app/main/map/map.module.ts
+++ src/app/main/map/map.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapRoutingModule } from './map-routing.module';
 import {MapComponent} from "./map.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [MapComponent],
-  imports: [
-    CommonModule,
-    MapRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        MapRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class MapModule { }
